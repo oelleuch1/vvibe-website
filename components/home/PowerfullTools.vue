fefbcf2de26e070e0a77dd7dea8e20c4c29aaf7e
OussamaELLEUCH <oussama.elleuch@outlook.com>
2022-05-01 16:01:06 +0200

    add powerfull tools

---
+++ components/home/PowerfullTools.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="powerfull-tools">
+    <h2 class="powerfull-tools__title">Powerful toolset and filtering</h2>
+    <div class="powerfull-tools__description">
+      Vvibe allows you to send messages directly to the people who want to hear
+      from you. Our filtering lets you select demographics and user info in a
+      variety of powerful combinations.
+    </div>
+    <base-image src="/img/powerfull-tools.svg" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import BaseImage from '../ui/atoms/BaseImage.vue'
+export default Vue.extend({
+  components: { BaseImage },
+  name: 'PowerfullTools',
+})
+</script>
+
+<style lang="scss" scoped>
+.powerfull-tools {
+  margin: 124px 0;
+
+
+  &__title {
+    font-family: 'Archivo Black';
+    font-style: normal;
+    font-weight: bolder;
+    font-size: 40px;
+    line-height: 50px;
+    text-transform: uppercase;
+  }
+
+  &__description {
+    font-family: 'Gilroy';
+    font-size: 18px;
+    line-height: 28px;
+    text-align: center;
+    max-width: 650px;
+    margin: 0 auto;
+    margin-bottom: 44px;
+  }
+
+  img {
+      width: 100%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <conversations-that-converts />
     <celebrities />
     <signup-steps />
+    <powerfull-tools />
   </div>
 </template>
 
@@ -13,10 +14,11 @@ import MainContent from "@/components/home/MainContent.vue";
 import ConversationsThatConverts from "~/components/home/ConversationsThatConverts.vue";
 import Celebrities from "~/components/home/Celebrities.vue";
 import SignupSteps from "~/components/home/SignupSteps.vue";
+import PowerfullTools from "~/components/home/PowerfullTools.vue";
 
 export default Vue.extend({
   name: "HomeView",
-  components: { MainContent, ConversationsThatConverts, Celebrities, SignupSteps },
+  components: { MainContent, ConversationsThatConverts, Celebrities, SignupSteps, PowerfullTools },
 });
 </script>
 
